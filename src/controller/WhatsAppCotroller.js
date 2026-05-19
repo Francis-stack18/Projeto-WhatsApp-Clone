cef523d005e02b83050cd758e0016155cd109418
Francis <francis13322@gmail.com>
2026-05-19 16:37:13 -0300

    W04 - Eventos de abrir e fechar painéis

--- src/controller/WhatsAppCotroller.js
+++ src/controller/WhatsAppCotroller.js
@@ -2,6 +2,7 @@ class WhatsAppCotroller {
   constructor() {
     this.elementsPrototype();
     this.loadElements();
+    this.initEvents();
   }
 
   loadElements() {
@@ -59,5 +60,36 @@ class WhatsAppCotroller {
     Element.prototype.hasClass = function (name) {
       return this.classList.contains(name);
     };
+  }
+
+  initEvents() {
+    this.el.myPhoto.on("click", (e) => {
+      this.closeAllLeftPanel();
+      this.el.panelEditProfile.show();
+      setTimeout(() => {
+        this.el.panelEditProfile.addClass("open");
+      }, 300);
+    });
+
+    this.el.btnNewContact.on("click", (e) => {
+      this.closeAllLeftPanel();
+      this.el.panelAddContact.show();
+      setTimeout(() => {
+        this.el.panelAddContact.addClass("open");
+      }, 300);
+    });
+
+    this.el.btnClosePanelEditProfile.on("click", (e) => {
+      this.el.panelEditProfile.removeClass("open");
+    });
+
+    this.el.btnClosePanelAddContact.on("click", (e) => {
+      this.el.panelAddContact.removeClass("open");
+    });
+  }
+
+  closeAllLeftPanel() {
+    this.el.panelAddContact.hide();
+    this.el.panelEditProfile.hide();
   }
 }
