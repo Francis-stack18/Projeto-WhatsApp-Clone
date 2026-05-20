63d331661bc8c3539f6107251212542484054ad5
Francis <francis13322@gmail.com>
2026-05-20 18:13:40 -0300

    W13 - Ativando a camera com API MediaDevices

--- src/controller/WhatsAppCotroller.js
+++ src/controller/WhatsAppCotroller.js
@@ -151,6 +151,7 @@ class WhatsAppCotroller {
       this.el.panelCamera.css({
         height: "calc(100% - 120px)",
       });
+      this._camera = new CameraController(this.el.videoCamera)
     });
 
     this.el.btnClosePanelCamera.on("click", (e) => {
