dd2fface21f615bf3c5dd8701113f25e13148e25
Francis <francis13322@gmail.com>
2026-05-20 17:05:02 -0300

    W10 - Eventos do campo Digitar

--- src/controller/WhatsAppCotroller.js
+++ src/controller/WhatsAppCotroller.js
@@ -200,13 +200,57 @@ class WhatsAppCotroller {
     this.el.btnFinishMicrophone.on("click", (e) => {
       this.closeRecordMicrophone();
     });
+
+    this.el.inputText.on("keypress", (e) => {
+      if (e.key === "Enter" && !e.ctrlKey) {
+        e.preventDefault();
+        this.el.btnSend.click();
+      }
+    });
+
+    this.el.inputText.on("keyup", (e) => {
+      if (this.el.inputText.innerHTML.length) {
+        this.el.inputPlaceholder.hide();
+        this.el.btnSendMicrophone.hide();
+        this.el.btnSend.show();
+      } else {
+        this.el.inputPlaceholder.show();
+        this.el.btnSendMicrophone.show();
+        this.el.btnSend.hide();
+      }
+    });
+
+    this.el.btnSend.on("click", e=>{
+
+      console.log("")
+
+    })
+
+    this.el.btnEmojis.on("click", e=>{
+
+      this.el.panelEmojis.toggleClass("open")
+      
+    })
+
+    this.el.panelEmojis.querySelectorAll(".emojik").forEach(emoji=>{
+
+      emoji.on("click",e=>{
+
+        console.log("")
+
+      })
+
+    })
+
   }
 
   startRecordMicrophoneTime() {
     let start = Date.now();
 
     this._recordMicrophoneInterval = setInterval(() => {
-      this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
+      this.el.recordMicrophoneTimer.innerHTML = Format.toTime(
+        Date.now() - start,
+      );
     }, 100);
   }
 
