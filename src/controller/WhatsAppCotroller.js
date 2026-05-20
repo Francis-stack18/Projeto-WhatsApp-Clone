dc1ec8c97522cedd75425d86069508aece3d0183
Francis <francis13322@gmail.com>
2026-05-20 18:02:05 -0300

    W12 - Inserir Emoji getSelection(), createRange() e DocumentFragment()

--- src/controller/WhatsAppCotroller.js
+++ src/controller/WhatsAppCotroller.js
@@ -239,7 +239,22 @@ class WhatsAppCotroller {
         emoji.classList.forEach((name) => {
           img.classList.add(name);
         });
-        this.el.inputText.appendChild(img);
+        let cursor = window.getSelection();
+
+        if (!cursor.focusNode || !cursor.focusNode.id == "input-text") {
+          this.el.inputText.focus();
+          cursor = window.getSelection();
+        }
+
+        let range = document.createRange();
+
+        range = cursor.getRangeAt(0);
+        range.deleteContents();
+
+        let frag = document.createDocumentFragment();
+        frag.appendChild(img);
+        range.insertNode(frag);
+        range.setStartAfter(img);
         this.el.inputText.dispatchEvent(new Event("keyup"));
       });
     });
