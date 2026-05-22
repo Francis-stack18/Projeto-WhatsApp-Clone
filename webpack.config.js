b1954df4c25d89f05c388bbe3eaeacd56cca0692
Francis <francis13322@gmail.com>
2026-05-21 23:48:42 -0300

    W14 - Arquivo de configuração do Webpack - webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./"),
+    },
+    port: 8080,
+    open: true,
+  },
+};
