7c12a170e3948492dced508f2b2449f831f15974
Francis <francis13322@gmail.com>
2026-05-24 17:49:13 -0300

    W24 - Criando evento para visualizar o tempo de gravacao

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -61,6 +61,7 @@ export class MicrophoneController extends ClassEvent {
       });
 
       this._mediaRecorder.start();
+      this.startTimer();
     }
   }
 
@@ -68,6 +69,18 @@ export class MicrophoneController extends ClassEvent {
     if (this.isAvailable()) {
       this._mediaRecorder.stop();
       this.stop();
+      this.stopTimer();
     }
+  }
+
+  startTimer() {
+    let start = Date.now();
+    this._recordMicrophoneInterval = setInterval(() => {
+      this.trigger("recordtimer", Date.now() - start);
+    }, 100);
+  }
+
+  stopTimer() {
+    clearInterval(this._recordMicrophoneInterval);
   }
 }
--- src/controller/WhatsAppCotroller.js
+++ src/controller/WhatsAppCotroller.js
@@ -274,13 +274,15 @@ export class WhatsAppCotroller {
     this.el.btnSendMicrophone.on("click", (e) => {
       this.el.recordMicrophone.show();
       this.el.btnSendMicrophone.hide();
-      this.startRecordMicrophoneTime();
 
       this._microphoneController = new MicrophoneController();
 
       this._microphoneController.on("ready", (audio) => {
         console.log("Ready Event");
         this._microphoneController.startRecorder();
+      });
+      this._microphoneController.on("recordtimer", (timer) => {
+        this.el.recordMicrophoneTimer.innerHTML = Format.toTime(timer);
       });
     });
 
@@ -353,20 +355,9 @@ export class WhatsAppCotroller {
     });
   }
 
-  startRecordMicrophoneTime() {
-    let start = Date.now();
-
-    this._recordMicrophoneInterval = setInterval(() => {
-      this.el.recordMicrophoneTimer.innerHTML = Format.toTime(
-        Date.now() - start,
-      );
-    }, 100);
-  }
-
   closeRecordMicrophone() {
     this.el.recordMicrophone.hide();
     this.el.btnSendMicrophone.show();
-    clearInterval(this._recordMicrophoneInterval);
   }
 
   closeAllMainPanel() {
