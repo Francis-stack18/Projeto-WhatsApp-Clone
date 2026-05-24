a922f26937350ab73b8a3a4d463733f66a3a5fb9
Francis <francis13322@gmail.com>
2026-05-24 15:43:51 -0300

    W21 - Ativando o microfone com API MediaDevices

--- src/controller/WhatsAppCotroller.js
+++ src/controller/WhatsAppCotroller.js
@@ -1,5 +1,6 @@
 import { Format } from "../utils/Format";
 import { CameraController } from "./CameraController";
+import { MicrophoneController } from "./MicrophoneController.js";
 import { DocumentPreviewController } from "./DocumentPreviewController";
 
 export class WhatsAppCotroller {
@@ -274,13 +275,17 @@ export class WhatsAppCotroller {
       this.el.recordMicrophone.show();
       this.el.btnSendMicrophone.hide();
       this.startRecordMicrophoneTime();
+
+      this._microphoneController = new MicrophoneController();
     });
 
     this.el.btnCancelMicrophone.on("click", (e) => {
+      this._microphoneController.stop();
       this.closeRecordMicrophone();
     });
 
     this.el.btnFinishMicrophone.on("click", (e) => {
+      this._microphoneController.stop();
       this.closeRecordMicrophone();
     });
 
