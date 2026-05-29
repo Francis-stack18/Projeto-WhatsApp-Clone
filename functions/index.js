ab02168f7a24086eea3d2db3b20dabc0dd73cdad
Francis <francis13322@gmail.com>
2026-05-28 22:22:53 -0300

    W49 - Criando funcao que atualiza a ultima mensagem enviada

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,63 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions/v1");
+const admin = require("firebase-admin");
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
+admin.initializeApp();
+const db = admin.firestore();
+
 setGlobalOptions({maxInstances: 10});
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const encode64 = (input) => {
+  return Buffer.from(escape(input), "utf8").toString("base64");
+};
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const decode64 = (input) => {
+  return unescape(Buffer.from(input, "base64").toString("utf8"));
+};
+
+exports.saveLastMessage = functions.firestore
+    .document(`/chats/{chatId}/messages/{messageId}`)
+    .onCreate((snap, context) => {
+      const chatId = context.params.chatId;
+      const messageId = context.params.messageId;
+
+      const messageDoc = snap.data();
+      const userFrom = messageDoc.from;
+      const chatRef = db.collection("chats").doc(chatId);
+
+      return chatRef.get().then((snapChat) => {
+        const chatDoc = snapChat.data();
+
+        const userTo = Object.keys(chatDoc.users).filter((key) => {
+          return key !== encode64(userFrom);
+        })[0];
+
+        const updateReceiver = db
+            .collection("users")
+            .doc(decode64(userTo))
+            .collection("contacts")
+            .doc(encode64(userFrom))
+            .set(
+                {
+                  lastMessage: messageDoc.content,
+                  lastMessageTime: new Date(),
+                },
+                {merge: true},
+            );
+
+        const updateSender = db
+            .collection("users")
+            .doc(userFrom)
+            .collection("contacts")
+            .doc(userTo)
+            .set(
+                {
+                  lastMessage: messageDoc.content,
+                  lastMessageTime: new Date(),
+                },
+                {merge: true},
+            );
+
+        return Promise.all([updateReceiver, updateSender]);
+      });
+    });
