b5519c2e66ffaf82ea07a6707364a2c585ba0f91
Francis <francis13322@gmail.com>
2026-05-28 23:03:26 -0300

    W50 - Notificacao desktop de novas mensagens

--- src/controller/WhatsAppCotroller.js
+++ src/controller/WhatsAppCotroller.js
@@ -12,11 +12,47 @@ import { Upload } from "../utils/Upload";
 
 export class WhatsAppCotroller {
   constructor() {
+    this._active = true;
     this._firebase = new Firebase();
     this.initAuth();
     this.elementsPrototype();
     this.loadElements();
     this.initEvents();
+    this.checkNotifications();
+  }
+
+  checkNotifications() {
+    if (typeof Notification === "function") {
+      if (Notification.permission !== "granted") {
+        this.el.alertNotificationPermission.show();
+      } else {
+        this.el.alertNotificationPermission.hide();
+      }
+
+      this.el.alertNotificationPermission.on("click", (e) => {
+        Notification.requestPermission((permission) => {
+          if (permission === "granted") {
+            this.el.alertNotificationPermission.hide();
+          }
+        });
+      });
+    }
+  }
+
+  notification(data) {
+    if (Notification.permission === "granted" && !this._active) {
+      let n = new Notification(this._contactActive.name, {
+        icon: this._contactActive.photo,
+        body: data.content,
+      });
+
+      let sound = new Audio("./audio/alert.mp3");
+      sound.currentTime = 0;
+      sound.play();
+      setTimeout(() => {
+        if (n) n.close();
+      }, 3000);
+    }
   }
 
   initAuth() {
@@ -158,6 +194,8 @@ export class WhatsAppCotroller {
 
     this.el.panelMessagesContainer.innerHTML = "";
 
+    this._messagesReceived = [];
+
     Message.getRef(this._contactActive.chatId)
       .orderBy("timeStamp")
       .onSnapshot((docs) => {
@@ -175,6 +213,16 @@ export class WhatsAppCotroller {
           message.fromJSON(data);
 
           let me = data.from === this._user.email;
+
+          if (
+            !me &&
+            this._messagesReceived.filter((id) => {
+              return id === data.id;
+            }).length === 0
+          ) {
+            this.notification(data);
+            this._messagesReceived.push(data.id);
+          }
 
           let view = message.getViewElement(me);
 
@@ -318,6 +366,14 @@ export class WhatsAppCotroller {
   }
 
   initEvents() {
+    window.addEventListener("focus", (e) => {
+      this._active = true;
+    });
+
+    window.addEventListener("blur", (e) => {
+      this._active = false;
+    });
+
     this.el.inputSearchContacts.on("keyup", (e) => {
       if (this.el.inputSearchContacts.value.lenght > 0) {
         this.el.inputSearchContactsPlaceholder.hide();
